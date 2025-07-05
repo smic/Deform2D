6985ae1212ce79aef3450305abfcdd75c80aac22
Stephan Michels <stephan.michels@gmail.com>
2025-07-05 09:49:22 +0200

    Initial Commit

---
+++ Deform2D/Deform2DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deform2DApp.swift
+//  Deform2D
+//
+//  Created by Stephan Michels on 05.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Deform2DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
